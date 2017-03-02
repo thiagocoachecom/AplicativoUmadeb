c333e332fcc957cd7c3899a04174df8ebfa7f5f0
Thiago Soares Pereira <ths.pereira@gmail.com>
2017-03-02 16:38:48 -0300

    Teste de Commmit 2

--- app/src/main/java/br/com/netcriativa/umadeb/MainActivity.java
+++ app/src/main/java/br/com/netcriativa/umadeb/MainActivity.java
@@ -7,8 +7,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        // Comuias UMDEB
+
+
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
